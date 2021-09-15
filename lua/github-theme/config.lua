6c5ae54086035ac0ebaa6109145a744a5c8d7a69
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-09-15 18:24:26 +0530

    feat: vim config support with "vim.g"

--- lua/github-theme/config.lua
+++ lua/github-theme/config.lua
@@ -4,23 +4,30 @@ local config
 -- shim vim for kitty and other generators
 vim = vim or {g = {}, o = {}}
 
-vim.g.github_theme_style = "dark"
+local function opt(key, default)
+  key = "github_" .. key
+  if vim.g[key] == nil then
+    return default
+  else
+    return vim.g[key]
+  end
+end
 
 config = {
-  theme_style = vim.g.github_theme_style,
-  dark_float = true,
-  dark_sidebar = true,
-  transparent = false,
-  comment_style = "italic",
-  keyword_style = "italic",
-  function_style = "NONE",
-  variable_style = "NONE",
-  msg_area_style = "NONE",
-  hide_inactive_statusline = false,
-  hide_end_of_buffer = true,
-  sidebars = {},
-  colors = {},
-  dev = false,
+  theme_style = opt("theme_style", "dark"),
+  dark_float = opt("dark_float", false),
+  dark_sidebar = opt("dark_sidebar", true),
+  transparent = opt("transparent", false),
+  comment_style = opt("comment_style", "italic"),
+  keyword_style = opt("keyword_style", "italic"),
+  function_style = opt("function_style", "NONE"),
+  variable_style = opt("variable_style", "NONE"),
+  msg_area_style = opt("msg_area_style", "NONE"),
+  hide_inactive_statusline = opt("hide_inactive_statusline", true),
+  hide_end_of_buffer = opt("hide_end_of_buffer", true),
+  sidebars = opt("sidebars", {}),
+  colors = opt("colors", {}),
+  dev = opt("dev", false),
   transform_colors = false
 }
 
@@ -30,9 +37,8 @@ local function applyConfiguration(userConfig)
     if value ~= nil then
       if config[key] ~= nil then
         config[key] = value
-        if key == "theme_style" and value ~= vim.g.github_theme_style then vim.g.github_theme_style = value end
       else
-        error("projekt0n/github-nvim-theme: Option " .. key .. " does not exist") -- luacheck: ignore
+        error("projekt0n/github-nvim-theme: Config " .. key .. " does not exist") -- luacheck: ignore
       end
     end
   end
