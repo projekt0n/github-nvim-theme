1a04a404afeea9c752e6309739293e42db254e4b
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-10-19 14:00:41 +0200

    Less intruding colors

--- lua/github-theme/plugins/lualine.lua
+++ lua/github-theme/plugins/lualine.lua
@@ -11,31 +11,31 @@ lualine.build_lualine_theme = function(c)
   local tint_lualine_group = function(color)
     local group = {
       a = { bg = color, fg = c.bg },
-      b = { bg = util.darken(color, 0.2), fg = util.lighten(color, 0.2) },
+      b = { bg = util.darken(color, 0.05), fg = util.lighten(color, 0.05) },
     }
     if vim.o.background == 'dark' then
       group.c = {
         bg = util.darken(color, 0.01, c.bg2),
-        fg = util.lighten(color, 0.4, c.fg),
+        fg = util.lighten(color, 0.1, c.fg),
       }
     else
       -- inverting colors for light colorschemes
       group.c = {
         bg = util.lighten(color, 0.01, c.bg2),
-        fg = util.darken(color, 0.4, c.fg),
+        fg = util.darken(color, 0.1, c.fg),
       }
     end
     return group
   end
 
-  local inactive_hi = { bg = c.bg2, fg = util.darken(c.fg, 0.3) }
+  local inactive_hi = { bg = c.bg2, fg = util.darken(c.fg, 0.1) }
   return {
     normal = tint_lualine_group(c.blue),
-    insert = tint_lualine_group(c.green),
+    insert = tint_lualine_group(c.bright_green),
     command = tint_lualine_group(c.bright_magenta),
-    visual = tint_lualine_group(c.yellow),
-    replace = tint_lualine_group(c.red),
-    terminal = tint_lualine_group(c.orange),
+    visual = tint_lualine_group(c.bright_yellow),
+    replace = tint_lualine_group(c.bright_red),
+    terminal = tint_lualine_group(c.bright_blue),
     inactive = {
       a = inactive_hi,
       b = inactive_hi,
