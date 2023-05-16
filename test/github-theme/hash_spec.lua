9b4ab057240785ee84b4171a92ece4cec8988d69
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2023-05-16 09:02:37 +0530

    ci: Check theme integration with neovim

---
+++ test/github-theme/hash_spec.lua
@@ -0,0 +1,35 @@
+local hash = require('github-theme.lib.hash')
+
+describe('Hash', function()
+  it('should produce same result with different table order', function()
+    local t1 = { Normal = { bg = '#192330', fg = '#cdcecf' } }
+    local t2 = { Normal = { fg = '#cdcecf', bg = '#192330' } }
+    assert.is.same(hash(t1), hash(t2))
+  end)
+
+  it('should understand booleans', function()
+    local t1 = {
+      transparent = false,
+      terminal_colors = true,
+      dim_inactive = false,
+      module_default = true,
+      inverse = {
+        match_paren = false,
+        visual = false,
+        search = false,
+      },
+    }
+    local t2 = {
+      transparent = true,
+      terminal_colors = true,
+      dim_inactive = false,
+      module_default = false,
+      inverse = {
+        match_paren = true,
+        visual = false,
+        search = false,
+      },
+    }
+    assert.is_not.same(hash(t1), hash(t2))
+  end)
+end)
