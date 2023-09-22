23760051cf8c0921a4d129f18f23c705e41b2fb6
ful1e5 <ful1e5@users.noreply.github.com>
2023-09-22 19:47:25 +0000

    Update color primitives

--- lua/github-theme/palette/primitives/light_high_contrast.lua
+++ lua/github-theme/palette/primitives/light_high_contrast.lua
@@ -613,7 +613,7 @@ local M = vim.json.decode(
   { luanil = { object = false, array = false } }
 )
 M._VERSION =
-  vim.json.decode([=["7.13.0"]=], { luanil = { object = false, array = false } })
+  vim.json.decode([=["7.13.1"]=], { luanil = { object = false, array = false } })
 M._LICENSE = [=[
 The MIT License (MIT)
 
