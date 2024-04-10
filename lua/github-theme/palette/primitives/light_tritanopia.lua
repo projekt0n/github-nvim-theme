5d2d4654092cf994536e8da43fcd726264556df8
ful1e5 <ful1e5@users.noreply.github.com>
2024-04-10 19:47:17 +0000

    Update color primitives

--- lua/github-theme/palette/primitives/light_tritanopia.lua
+++ lua/github-theme/palette/primitives/light_tritanopia.lua
@@ -617,7 +617,7 @@ local M = vim.json.decode(
   { luanil = { object = false, array = false } }
 )
 M._VERSION =
-  vim.json.decode([=["7.15.6"]=], { luanil = { object = false, array = false } })
+  vim.json.decode([=["7.16.0"]=], { luanil = { object = false, array = false } })
 M._LICENSE = [=[
 The MIT License (MIT)
 
