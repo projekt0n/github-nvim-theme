54e61d00f34c26a5e4d2ebfa6fc641b2d7c75d11
Tyler Miller <tmillr@proton.me>
2024-07-18 14:44:32 -0700

    refactor(palette, spec): cleanup, DRY, add types

--- lua/github-theme/spec.lua
+++ lua/github-theme/spec.lua
@@ -1,9 +1,8 @@
-local collect = require('github-theme.lib.collect')
-local template = require('github-theme.util.template')
+local M = {}
 
 --#region Types
 
----@class Spec
+---@class GhTheme.Spec
 ---@field bg0 string
 ---@field bg1 string
 ---@field bg2 string
@@ -16,18 +15,17 @@ local template = require('github-theme.util.template')
 ---@field sel0 string
 ---@field sel1 string
 ---@field sel2 string
----@field syntax SpecSyntax
----@field diag SpecDiagnostic
----@field diag_bg SpecDiagnosticBg
----@field diff SpecDiff
----@field git SpecGit
+---@field syntax GhTheme.Spec.Syntax
+---@field diag GhTheme.Spec.Diagnostic
+---@field diag_bg GhTheme.Spec.DiagnosticBg
+---@field diff GhTheme.Spec.Diff
+---@field git GhTheme.Spec.Git
 
----@class SpecSyntax
+---@class GhTheme.Spec.Syntax
 ---@field bracket string
 ---@field builtin0 string
 ---@field builtin1 string
 ---@field builtin2 string
----@field builtin3 string
 ---@field comment string
 ---@field conditional string
 ---@field const string
@@ -47,64 +45,62 @@ local template = require('github-theme.util.template')
 ---@field type string
 ---@field variable string
 
----@class SpecDiagnostic
+---@class GhTheme.Spec.Diagnostic
 ---@field error string
 ---@field warn string
 ---@field info string
 ---@field hint string
 
----@class SpecDiagnosticBg
+---@class GhTheme.Spec.DiagnosticBg
 ---@field error string
 ---@field warn string
 ---@field info string
 ---@field hint string
 
----@class SpecDiff
+---@class GhTheme.Spec.Diff
 ---@field add string
 ---@field delete string
 ---@field change string
 ---@field text string
 
----@class SpecGit
+---@class GhTheme.Spec.Git
 ---@field add string
 ---@field removed string
 ---@field changed string
 
 --#endregion
 
-local M = {}
-
-local function override(spec, palette, ovr)
-  ovr = template.parse(ovr, palette)
-  return collect.deep_extend(spec, ovr)
+---@param theme GhTheme.Theme
+local function get_spec(theme)
+  local template = require('github-theme.util.template')
+  local ovr = require('github-theme.override').specs
+  local pal = require('github-theme.palette').load(theme)
+  local spec = pal.generate_spec(pal)
+  if ovr.all then
+    pal = vim.tbl_deep_extend('force', pal, template.parse(ovr.all, pal))
+  end
+  if ovr[theme] then
+    pal = vim.tbl_deep_extend('force', pal, template.parse(ovr[theme], pal))
+  end
+  spec.palette = pal
+  return spec
 end
 
-function M.load(name)
-  local ovr = require('github-theme.override').specs
-
-  local function apply_ovr(key, spec, palette)
-    return ovr[key] and override(spec, palette, ovr[key]) or spec
-  end
-
-  if name then
-    local palette = require('github-theme.palette').load(name)
-    local spec = palette.generate_spec(palette)
-    spec = apply_ovr('all', spec, palette)
-    spec = apply_ovr(name, spec, palette)
-    spec.palette = palette
-    return spec
+---Returns the spec for the given `theme`, or all themes (i.e. a map from theme
+---name to spec) if `theme` is `nil`.
+---@param theme? GhTheme.Theme
+function M.load(theme)
+  if theme ~= nil then
+    return get_spec(theme)
   else
-    local result = {}
-    local themes = require('github-theme.palette').themes
-    for _, mod in ipairs(themes) do
-      local palette = require('github-theme.palette').load(mod)
-      local spec = palette.generate_spec(palette)
-      spec = apply_ovr('all', spec, palette)
-      spec = apply_ovr(mod, spec, palette)
-      spec.palette = palette
-      result[mod] = spec
+    local all = {}
+
+    ---@diagnostic disable-next-line: redefined-local
+    for theme in pairs(M.themes) do
+      all[theme] = get_spec(theme)
     end
-    return result
+
+    return all
   end
 end
 
